3467e591c0d76172b81388aac2935bcb4ef5dd60
user <glenn@vassallo.com>
2013-09-15 22:57:47 +1000

    Beginning the Alarm Clock and Timer feature

---
+++ lightcontroller/SSAlarmClockViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSAlarmClockViewController.h
+//  LightMobile
+//
+//  Created by Glenn Vassallo on 15/09/13.
+//  Copyright (c) 2013 Swift Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAlarmClockViewController : UIViewController
+
+@end
---
+++ lightcontroller/SSAlarmTimerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSAlarmTimerViewController.h
+//  LightMobile
+//
+//  Created by Glenn Vassallo on 15/09/13.
+//  Copyright (c) 2013 Swift Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAlarmTimerViewController : UIViewController
+
+@end
